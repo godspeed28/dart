9acb39139585502520fd92e32a431a400f2bc1f0
godspeed28 <albertog4taz28@gmail.com>
2025-07-28 23:46:24 +0700

    init

---
+++ bin/variabel.dart
@@ -0,0 +1,23 @@
+import 'dart:ffi';
+
+void main() {
+  String firstName = 'Abe';
+  String lastName;
+  lastName = 'Kolin';
+
+  final fullName = '$firstName $lastName';
+
+  var age = 21;
+
+  print('Name: $fullName');
+  print('Age: $age years old');
+
+  final array1 = [1, 2, 3];
+  const array2 = [1, 2, 3];
+
+  array1[0] = 10; // Bisa diubah
+  // array2[0] = 10; // Tidak bisa diubah, akan error
+
+  print(array1);
+  print(array2);
+}
